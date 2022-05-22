cbb7f25d9dc655525b226205fe9509156988993d
Wiskey666 <47121348+Wiskey666@users.noreply.github.com>
2022-05-22 15:11:51 +0300

    Update a_0.2!
    [+] Output the number of key variants that can be specified in 8-, 16-, 32-, 64-, 128-, 256-, 512-, 1024-, 2048-, 4096-bit sequence.
    [+] For each of the options, it is necessary to generate a random key value that belongs to the range from 0x00…0 to 0xFF…F, depending on the selected key length.
    [+] Write a function to brute force values ​​from a range to find a key. The purpose of the function is to iterate over the key values ​​from 0x00...0 until a value is found that is equal to the pre-generated key. The function should output the amount of time, in milliseconds, that it took to find the key.

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
-import math
 import random
 import os
+import time
+import itertools
 from array import *
 
-#====================generating Functions====================
+#====================Functions====================
 def generateOptions(bits_length):
     print("------------------------------Number of key options------------------------------\n")
     for i in bits_length:
         try:
             res = (2**i)
             symbolsInLine = len(str(res))
-            print(f"{i}-bits = {res} [{symbolsInLine}]")
+            print(f"{i}-bits = {res} [{symbolsInLine}]\n")
         except OverflowError:
             print(f"{i}-bits = inf")
 
-def generateKeys(bits_length):
+def generateKeys(bits_length, key_dict):
     print("\n------------------------------Generated keys------------------------------\n")
     for i in bits_length:
         try:
-            key = random.getrandbits(i);
-            print(f"=========={i}-bit encryption key==========\n{key}\n=========================================\n\n")
+            key = random.getrandbits(i)
+            key_dict[int(key)] = key
+            print(f"=========={i}-bit encryption key==========\n{key}\n=========================================\n")
         except OverflowError:
             print(f"{i}-bits = inf")
 
+def brute_force(key_dict):
+    limit = 2**4096
+    key_found = 0
+    start = time.monotonic_ns()
+    for i in range(limit):
+        search_key = key_dict.get(i, False)
+        if search_key:
+            finish=time.monotonic_ns() 
+            duration = finish -  start
+            print(f"[+] Key {i} picked up in {duration//1000000}ms")
+            key_found += 1
+
+    print(f"[!] Total Keys found: {key_found}")
+
 #====================Main====================
 bits_length = array('i', [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096])
+key_dict = dict()
+
 generateOptions(bits_length)
-generateKeys(bits_length)
+generateKeys(bits_length, key_dict)
+brute_force(key_dict)
